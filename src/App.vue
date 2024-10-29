9fb11ff91ed43276bcdc45306976d8583b0d77b0
Suwanruch ARC <117613220+suwanruch-arc@users.noreply.github.com>
2024-10-29 17:30:47 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <main>
+  <main v-if="$q.platform.is.mobile">
     <RouterView />
   </main>
+  <div v-else class="q-pa-sm text-red">
+    ลูกค้าจะต้องทำรายการจากโทรศัพท์มือถือที่มีหมายเลขที่ลงทะเบียนเท่านั้น
+    และไม่เชื่อมต่อสัญญาณ WIFI
+  </div>
 </template>
